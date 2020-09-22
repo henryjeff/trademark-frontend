7c3fc06f2ac6cfde3150fc471d0a028d05dabe82
henryjeff <henryheffernan@gmail.com>
2020-09-22 15:12:47 -0400

    [update] Added navbar and fixed some styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ import KeyIcon from "./assets/icons/Key.svg";
 import { Button } from "./components/button/";
 import { TextInput } from "./components/input/";
 import {
+  Navbar,
+  NavbarSection,
+  NavbarOption,
+} from "./components/view/navigation";
+import {
   TitleText,
   HeaderText,
   Subtext,
@@ -21,54 +26,80 @@ function App() {
 
   return (
     <div className="app">
-      <div style={styles.form}>
-        {/* <TitleText>TITLE TEXT</TitleText>
-        <HeaderText>HEADING 1</HeaderText>
-        <HeaderText small>HEADING 2</HeaderText>
-        <Text>Regular Text</Text>
-        <Subtext containerStyles={{ marginBottom: 20 }}>Subtext</Subtext> */}
-        <TitleText>SIGN UP</TitleText>
-        <HeaderText
-          small
-          light
-          containerStyles={{ marginBottom: 20 }}
-          color={colors.gray1}
-        >
-          Welcome to the club
-        </HeaderText>
-        <TextInput
-          placeholderText="Username"
-          onChange={(e) => {
-            console.log(e.target.value);
-          }}
-        />
-        <TextInput
-          placeholderText="Email"
-          onChange={(e) => {
-            console.log("e.target.value");
-          }}
-        />
-        <TextInput
-          inputType="password"
-          placeholderText="Password"
-          onChange={(e) => {
-            console.log("e.target.value");
-          }}
-        />
-        <TextInput
-          inputType="password"
-          placeholderText="Re-type Password"
-          onChange={(e) => {
-            console.log("e.target.value");
-          }}
-        />
-        <Button
-          isLoading={isLoading}
-          onClick={() => {
-            setIsLoading(true);
-          }}
-          buttonText="SIGN UP"
-        />
+      <Navbar>
+        <NavbarSection justify="left">
+          <NavbarOption>
+            <Text weight="medium">Dashboard</Text>
+          </NavbarOption>
+          <NavbarOption>
+            <Text weight="medium" color={colors.gray1}>
+              Getting Started
+            </Text>
+          </NavbarOption>
+          <NavbarOption>
+            <Text weight="medium" color={colors.gray1}>
+              Help
+            </Text>
+          </NavbarOption>
+        </NavbarSection>
+        <NavbarSection justify="center">
+          <NavbarOption>
+            <TitleText fontSize={24}>TM</TitleText>
+          </NavbarOption>
+        </NavbarSection>
+        <NavbarSection justify="right">
+          <NavbarOption>
+            <Text weight="medium" color={colors.gray1}>
+              henryjeff
+            </Text>
+          </NavbarOption>
+        </NavbarSection>
+      </Navbar>
+      <div style={styles.body}>
+        <div style={styles.form}>
+          {/* <TitleText>TITLE TEXT</TitleText>
+          <HeaderText>HEADING 1</HeaderText>
+          <HeaderText small>HEADING 2</HeaderText>
+          <Text>Regular Text</Text>
+          <Subtext containerStyles={{ marginBottom: 20 }}>Subtext</Subtext> */}
+          <TitleText>SIGN UP</TitleText>
+          <HeaderText small style={{ marginBottom: 20 }} color={colors.gray1}>
+            Welcome to the club
+          </HeaderText>
+          <TextInput
+            placeholderText="Username"
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+          />
+          <TextInput
+            placeholderText="Email"
+            onChange={(e) => {
+              console.log("e.target.value");
+            }}
+          />
+          <TextInput
+            inputType="password"
+            placeholderText="Password"
+            onChange={(e) => {
+              console.log("e.target.value");
+            }}
+          />
+          <TextInput
+            inputType="password"
+            placeholderText="Re-type Password"
+            onChange={(e) => {
+              console.log("e.target.value");
+            }}
+          />
+          <Button
+            isLoading={isLoading}
+            onClick={() => {
+              setIsLoading(true);
+            }}
+            buttonText="SIGN UP"
+          />
+        </div>
       </div>
     </div>
   );
@@ -76,6 +107,7 @@ function App() {
 
 const styles: StyleSheet = {
   form: {
+    marginTop: "10vh",
     width: "25%",
     textAlign: "center",
     justifyContent: "center",
@@ -83,6 +115,12 @@ const styles: StyleSheet = {
     display: "flex",
     flexDirection: "column",
     // backgroundColor: colors.red,
+  },
+  body: {
+    justifyContent: "center",
+    alignItems: "flex-start",
+    display: "flex",
+    flex: 1,
   },
 };
 
