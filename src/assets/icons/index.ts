780de0b964368a5890666d34870549a4fd8c921f
henryjeff <henryheffernan@gmail.com>
2020-09-25 16:44:46 -0400

    [add] New icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,8 +2,9 @@ import KeyIcon from "./Key.svg";
 import MailIcon from "./Mail.svg";
 import UserIcon from "./User.svg";
 import DownArrowIcon from "./DownArrow.svg";
+import SignOutIcon from "./SignOut.svg";
 
-export type IconName = "Key" | "Mail" | "User" | "DownArrow";
+export type IconName = "Key" | "Mail" | "User" | "DownArrow" | "SignOut";
 
 const icons: {
   [key in IconName]: string;
@@ -12,6 +13,7 @@ const icons: {
   Mail: MailIcon,
   User: UserIcon,
   DownArrow: DownArrowIcon,
+  SignOut: SignOutIcon,
 };
 
 const getIconByName = (iconName: IconName): string => icons[iconName];
